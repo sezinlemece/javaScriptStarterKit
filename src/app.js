00afc309d44b305bb40b008b3163e9f53fc6c552
sezinlemece <scoskunsu@yahoo.com>
2021-10-19 22:37:00 +0300

    19 Ekim DErs Sonu

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("Hello Sezin")
+
+let dolarDun = 9.29
+
+let dolarBugun =9.30
+
+
+// console.log(dolarDun)
+
+// const euroDun = 11.2
+
+// console.log(euroDun)
+
+// let konutKredileri = ["Konu kredisi", "Emlak kredisi", "Kamu kredisi"]
+
+// console.log(konutKredileri)
+
+for(let i=0;i<3;i++){
+console.log("<li>" + i + "</li>")
+}
+console.log("</ul>")
